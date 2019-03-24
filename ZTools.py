36402f26d87b0962b68465fe02428202dba7e062
zongzi <545670755@qq.com>
2019-03-24 23:58:36 +0800

    add init file

--- ZTools.py
+++ ZTools.py
@@ -1,13 +1,14 @@
 import sublime
+from sublime import Region
 import sublime_plugin 
 
 def insert_at_first(view,edit,position,content):
-	view.insert(edit,position,content)
+	view.insert(edit,position,content+"\n")
 
 
-class aCommand(sublime_plugin.TextCommand):
+class ToolsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		insert_at_first(self.view,edit,0,"aCommand")
-		line_all=self.Region(0,-1)
-		self.view.erase(line_all)
-		
+		insert_at_first(self.view,edit,0,"and")
+		line_all=Region(0,-1)
+		a = self.view.lines(line_all)
+		insert_at_first(self.view,edit,0,str(a))
